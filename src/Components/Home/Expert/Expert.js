5b81d616144d690b7537b1c6b424fb8535cee558
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-16 18:32:56 +0600

    expert section added

--- src/Components/Home/Expert/Expert.js
+++ src/Components/Home/Expert/Expert.js
@@ -6,21 +6,29 @@ import expert1 from '../../../images/expert1.png'
 
 function Expert() {
 	const bgImgStyle = {
-		background: 'url('+ expert1 + ')',
+		background: 'url(' + expert1 + ')',
 		backgroundRepeat: 'no-repeat',
 		backgroundPosition: 'center center',
-		backgroundSize: 'cover'
-
+		backgroundSize: 'cover',
+		width: '12rem',
+		height: '14rem',
+		position: 'relative'
 	}
 	return (
 		<div style={bgImgStyle} className='Expert'>
 			<div className="dummyContent">
 			</div>
-			<div className="content">
-				
+			<div className="content"></div>
+			<div className='text'>
+				<p className="name">
+					Andrea Luies
+				</p>
+				<p className="designation">
+					Head in Laundry
+				</p>
 			</div>
 		</div>
 	)
 }
 
-export default Expert
+export default Expert;
