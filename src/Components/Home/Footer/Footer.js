ec5e829166562a1af439281ec71a5bab3d78a0ca
arif-hstu <arif.dvm.hstu@gmail.com>
2021-04-16 23:37:28 +0600

    footer improved

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -21,52 +21,57 @@ import './Footer.css';
 
 function Footer() {
 	return (
-		<div className='Footer'>
-			<div className="about">
-				<h4>We are <span className='highlight'>Cleanex!</span></h4>
-				<p>We work with a passion of taking challenges and creating new ones in advertising sector.</p>
-				<h6>Open Hours:</h6>
-				<p>Sat-Thurs: 8 am - 5 pm. <br />Friday: CLOSED</p>
-			</div>
-
-			<div className="newsletter">
-				<h4>We are <span className='highlight'>Cleanex!</span></h4>
-				<p>We work with a passion of taking challenges and creating new ones in advertising sector.</p>
-				<div className="socialIcons">
-					<FontAwesomeIcon icon={faFacebookF} />
-					<FontAwesomeIcon icon={faTwitter} />
-					<FontAwesomeIcon icon={faYoutube} />
-					<FontAwesomeIcon icon={faInstagram} />
+		<>
+			<div className='Footer'>
+				<div className="about">
+					<h4>We are <span className='highlight'>Cleanex!</span></h4>
+					<p>We work with a passion of taking challenges and creating new ones in advertising sector.</p>
+					<h6>Open Hours:</h6>
+					<p>Sat-Thurs: 8 am - 5 pm. <br />Friday: CLOSED</p>
 				</div>
-			</div>
 
-			<div className="officialInfo">
-				<h4>We are <span className='highlight'>Cleanex!</span></h4>
-				<div className="contact">
-					<div>
-						<FontAwesomeIcon icon={faMapMarker} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
-					</div>
-					<div>
-						<FontAwesomeIcon icon={faPhone} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
-					</div>
-					<div>
-						<FontAwesomeIcon icon={faEnvelope} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
+				<div className="newsletter">
+					<h4>We are <span className='highlight'>Cleanex!</span></h4>
+					<p>We work with a passion of taking challenges and creating new ones in advertising sector.</p>
+					<div className="socialIcons">
+						<FontAwesomeIcon icon={faFacebookF} />
+						<FontAwesomeIcon icon={faTwitter} />
+						<FontAwesomeIcon icon={faYoutube} />
+						<FontAwesomeIcon icon={faInstagram} />
 					</div>
 				</div>
-			</div>
 
-			<div className="social">
-				<h4>Instagram</h4>
-				<div className="contact">
-					<img src={ insta1 } alt="" />
-					<img src={ insta2 } alt="" />
-					<img src={ insta3 } alt="" />
-					<img src={ insta4 } alt="" />
-					<img src={ insta5 } alt="" />
-					<img src={ insta6 } alt="" />
+				<div className="officialInfo">
+					<h4>We are <span className='highlight'>Cleanex!</span></h4>
+					<div className="contact">
+						<div>
+							<FontAwesomeIcon icon={faMapMarker} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
+						</div>
+						<div>
+							<FontAwesomeIcon icon={faPhone} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
+						</div>
+						<div>
+							<FontAwesomeIcon icon={faEnvelope} /> <p>Suihari, Dinajpur Sadar, Dinajpur,Dinajpur</p><br />
+						</div>
+					</div>
+				</div>
+
+				<div className="social">
+					<h4>Instagram</h4>
+					<div className="contact">
+						<img src={insta1} alt="" />
+						<img src={insta2} alt="" />
+						<img src={insta3} alt="" />
+						<img src={insta4} alt="" />
+						<img src={insta5} alt="" />
+						<img src={insta6} alt="" />
+					</div>
 				</div>
 			</div>
-		</div>
+			<div className="copyrightBar">
+				<p>2021 All rights reserved by Md Arifur Rahman</p>
+			</div>
+		</>
 	)
 }
 
